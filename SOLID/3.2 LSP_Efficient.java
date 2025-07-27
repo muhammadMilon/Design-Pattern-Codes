27c5249d7111ee3e2d76239c0586484918b1f251
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2025-07-27 17:03:02 +0600

    Update 3.2 LSP_Efficient.java

--- SOLID/3.2 LSP_Efficient.java
+++ SOLID/3.2 LSP_Efficient.java
@@ -23,12 +23,21 @@ class Ostrich extends NonFlyingBird {
     }
 }
 
+class Penguin extends NonFlyingBird {
+    @Override
+    public void move() {
+        System.out.println("Swimming...");
+    }
+}
+
 public class LSP_Efficient {
     public static void main(String[] args) {
         Bird bird1 = new FlyingBird();
         Bird bird2 = new Ostrich();
+        Bird bird3 = new Penguin();
 
         bird1.move(); // Output: Flying...
         bird2.move(); // Output: Running...
+        bird3.move(); // Output: Swimming...
     }
 }
